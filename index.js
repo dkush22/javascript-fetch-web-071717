86a2a597954434875301acbd64f75d9deca7c8ba
dkush22 <dkush22@gmail.com>
2017-09-11 16:03:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const app = "I don't do much.";
+
+fetch('https://api.github.com/repos/jquery/jquery/commits')
+  .then(res => res.json())
+  .then(json => console.log(json));
+
+
+const token = 'b363e815d7ec8894038ecbb22f1b473ed9a47e14'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
